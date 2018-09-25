d2b545523d559d2b1ca0d157dfb2b2b3f7e6d8ad
Lucas Tulio <lucasctulio@gmail.com>
2018-09-25 16:04:14 -0300

    Included button height variations, fix disabled and active combo

--- components/button/styles.js
+++ components/button/styles.js
@@ -2,22 +2,28 @@ import styled, { css } from 'styled-components'
 import COLORS from 'shared/colors'
 import MEASURES from 'shared/measures'
 
+const BUTTON_HEIGHT = {
+  tall: '60px',
+  medium: '40px'
+}
+
 const Button = styled.button`
   font-size: ${MEASURES.font.default};
   letter-spacing: ${MEASURES.defaultLetterSpacing};
-  height: 40px;
   padding: 8px 20px 8px 20px;
   border-radius: 4px;
   border: 1px solid;
   border-color: #e0e6ed;
   
-  ${props => props.active && css`
+  ${props => props.active && !props.disabled && css`
     color: ${COLORS.white};
     background-color: ${COLORS.pink};
     border-color: ${COLORS.pink};
   `}
 
-  cursor: ${props => props.disabled ? 'default' : 'pointer'}
+  height: ${(props) => props.height ? BUTTON_HEIGHT[props.height] : BUTTON_HEIGHT.medium};
+
+  cursor: ${props => props.disabled ? 'default' : 'pointer'};
 `
 
 export default Button
