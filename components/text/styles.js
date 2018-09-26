2366816150ada478fc73fc6f52132a2744085025
Lucas Tulio <lucasctulio@gmail.com>
2018-09-26 19:54:37 -0300

    Add text input component

--- components/text/styles.js
+++ components/text/styles.js
@@ -1,5 +1,8 @@
 import styled from 'styled-components'
+import COLORS from 'shared/colors'
 
-const View = styled.div``
+const View = styled.div`
+  color: ${props => props.color ? props.color : COLORS.dark};
+`
 
 export default View
