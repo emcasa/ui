4f271ff2fdfe310a6bc2e40ccaf45bda7f6b29a0
Lucas Tulio <lucasctulio@gmail.com>
2018-09-27 19:18:59 -0300

    Separate project structure into Components and Docs

--- components/text/styles.js
+++ components/text/styles.js
@@ -3,7 +3,6 @@ import COLORS from 'shared/colors'
 
 const Text = styled.p`
   color: ${props => props.color ? props.color : COLORS.dark};
-  font-family: 'FaktSoftPro-Normal';
 `
 
 export default Text
