5284f61a7303dc2ef1f7cb29a5564f6402062b0d
Lucas Tulio <lucasctulio@gmail.com>
2018-09-28 23:36:09 -0300

    Applying docz style

--- doczrc.js
+++ doczrc.js
@@ -1,6 +1,19 @@
 export default {
+  title: 'EmCasa UI',
   dest: './public',
   base: '/',
   hashRouter: true,
-  wrapper: 'wrapper.js'
+  wrapper: 'wrapper.js',
+  themeConfig: {
+    logo: {
+      src: '/logo.png',
+      width: 232
+    },
+    colors: {
+      primary: '#F50057',
+      text: '#3c4858',
+      sidebarBg: '#e0e6ed',
+      link: '#1fb6ff'
+    }
+  }
 }
