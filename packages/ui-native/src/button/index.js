7f46bb6f45713cd0330c1a4748998d88663c5ba7
Gabriela Seabra <gabriela.lima.se@gmail.com>
2018-10-04 12:50:13 -0300

    compile ui-native to components/

--- packages/ui-native/src/button/index.js
+++ packages/ui-native/src/button/index.js
@@ -1,7 +1,25 @@
 import React from 'react'
 import pick from 'lodash.pick'
+import styled from 'styled-components/native'
+import * as button from '@emcasa/ui/components/button'
 
-import Button, {ButtonText} from './styles'
+export const Button = styled.TouchableOpacity`
+  ${button.container};
+`
+
+Button.defaultProps = {
+  ...button.container.defaultProps,
+  alignItems: 'center',
+  justifyContent: 'center'
+}
+
+export const ButtonText = styled.Text`
+  ${button.text};
+`
+
+ButtonText.defaultProps = {
+  ...button.text.defaultProps
+}
 
 export default ({children, ...props}) => (
   <Button {...props}>
