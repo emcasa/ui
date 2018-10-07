9f29b847d7041c44e74d29a2754bcaf43e29c88b
Gabriela Seabra <gabriela.lima.se@gmail.com>
2018-10-06 22:56:24 -0300

    Setup ui-dom storybook

--- packages/ui-dom/.storybook/config.js
+++ packages/ui-dom/.storybook/config.js
@@ -1,7 +1,10 @@
-import {configure} from '@storybook/react'
+import {configure, setAddon} from '@storybook/react'
+import addonInfo from '@storybook/addon-info'
 
 // automatically import all files ending in *.stories.js
-const req = require.context('../stories', true, /.stories.js$/)
+const req = require.context('../src', true, /.stories.js$/)
+
+setAddon(addonInfo)
 
 function loadStories() {
   req.keys().forEach((filename) => req(filename))
--- packages/ui-dom/src/Button/Button.stories.js
+++ packages/ui-dom/src/Button/Button.stories.js
@@ -4,11 +4,7 @@ import {storiesOf} from '@storybook/react'
 import {action} from '@storybook/addon-actions'
 import {linkTo} from '@storybook/addon-links'
 
-import {Button, Welcome} from '@storybook/react/demo'
-
-storiesOf('Welcome', module).add('to Storybook', () => (
-  <Welcome showApp={linkTo('Button')} />
-))
+import {Button} from '@storybook/react/demo'
 
 storiesOf('Button', module)
   .add('with text', () => (
