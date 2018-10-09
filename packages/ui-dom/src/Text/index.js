6860390dfc1cff5714e3b154968a43efb4498dda
Lucas Tulio <lucasctulio@gmail.com>
2018-10-09 15:49:23 -0300

    Inline Text. Closes #27

--- packages/ui-dom/src/Text/index.js
+++ packages/ui-dom/src/Text/index.js
@@ -1,7 +1,13 @@
+import React from 'react'
 import styled from 'styled-components'
 import * as text from '@emcasa/ui/lib/components/Text'
 
-const Text = styled.p`
+const Text = styled((props) => {
+  // Remove the 'inline' prop that would be passed to the dom element
+  let domProps = Object.assign({}, props)
+  delete domProps.inline
+  return (props.inline ? <span {...domProps} /> : <p {...domProps} />)
+})`
   ${text.container};
 `
 
