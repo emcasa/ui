9498ff3952ee18e79753bba5c4696b88a9d2096e
Lucas Tulio <lucasctulio@gmail.com>
2018-10-09 18:33:34 -0300

    Move global styles to ui-dom

---
+++ packages/ui-dom/src/global-styles.js
@@ -0,0 +1,34 @@
+import { injectGlobal } from 'styled-components'
+
+injectGlobal`
+  @font-face {
+    font-family: "FaktSoftPro-Normal";
+    src: url("https://s3.amazonaws.com/emcasa-ui/fonts/FaktSoftPro-Normal.eot");
+    src: url("https://s3.amazonaws.com/emcasa-ui/fonts/FaktSoftPro-Normal.woff") format('woff'),
+        url("https://s3.amazonaws.com/emcasa-ui/fonts/FaktSoftPro-Normal.woff2") format('woff2'),
+        url("https://s3.amazonaws.com/emcasa-ui/fonts/FaktSoftPro-Normal.ttf") format('ttf');
+  }
+
+  @font-face {
+    font-family: "FaktSoftPro-Blond";
+    src: url("https://s3.amazonaws.com/emcasa-ui/fonts/FaktSoftPro-Blond.eot");
+    src: url("https://s3.amazonaws.com/emcasa-ui/fonts/FaktSoftPro-Blond.woff") format('woff'),
+        url("https://s3.amazonaws.com/emcasa-ui/fonts/FaktSoftPro-Blond.woff2") format('woff2'),
+        url("https://s3.amazonaws.com/emcasa-ui/fonts/FaktSoftPro-Blond.ttf") format('ttf');
+  }
+
+  @font-face {
+    font-family: "FaktSoftPro-Medium";
+    src: url("https://s3.amazonaws.com/emcasa-ui/fonts/FaktSoftPro-Medium.eot");
+    src: url("https://s3.amazonaws.com/emcasa-ui/fonts/FaktSoftPro-Medium.woff") format('woff'),
+        url("https://s3.amazonaws.com/emcasa-ui/fonts/FaktSoftPro-Medium.woff2") format('woff2'),
+        url("https://s3.amazonaws.com/emcasa-ui/fonts/FaktSoftPro-Medium.ttf") format('ttf');
+  }
+
+  * {
+    font-family: 'FaktSoftPro-Normal';
+    -webkit-font-smoothing: antialiased;
+  }
+`
+
+export default injectGlobal
