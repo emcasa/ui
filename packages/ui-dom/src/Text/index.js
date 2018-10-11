efc7931c0affe5079f6b1826507f7b3433ef223b
Lucas Tulio <lucasctulio@gmail.com>
2018-10-11 19:32:43 -0300

    Remove textAlign prop from dom element. Closes #35

--- packages/ui-dom/src/Text/index.js
+++ packages/ui-dom/src/Text/index.js
@@ -3,9 +3,10 @@ import styled from 'styled-components'
 import * as text from '@emcasa/ui/lib/components/Text'
 
 const Text = styled((props) => {
-  // Remove the 'inline' prop that would be passed to the dom element
+  // Remove props that shouldn't be passed to the dom element
   let domProps = Object.assign({}, props)
   delete domProps.inline
+  delete domProps.textAlign
   return (props.inline ? <span {...domProps} /> : <p {...domProps} />)
 })`
   ${text.container};
