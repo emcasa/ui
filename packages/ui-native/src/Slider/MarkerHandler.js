98c49c7166ae8760dad022987334a97462b37d52
Gabriela Seabra <gabriela.lima.se@gmail.com>
2018-12-12 23:11:39 -0200

    Emulate marker hitSlop on android

--- packages/ui-native/src/Slider/MarkerHandler.js
+++ packages/ui-native/src/Slider/MarkerHandler.js
@@ -1,8 +1,8 @@
 import React, {Component} from 'react'
-import {Animated} from 'react-native'
+import {View, Animated, Platform} from 'react-native'
 import {PanGestureHandler, State} from 'react-native-gesture-handler'
 
-export default class MarkerContainer extends Component {
+export default class MarkerHandler extends Component {
   static defaultProps = {
     zIndex: 100,
     hitSlop: 15
@@ -11,7 +11,7 @@ export default class MarkerContainer extends Component {
   state = {
     bounds: undefined,
     layout: undefined,
-    sliderState: State.UNDETERMINED
+    sliderState: State.UNDETERMINED,
   }
 
   position = new Animated.Value(0)
@@ -34,8 +34,11 @@ export default class MarkerContainer extends Component {
   }
 
   static getDerivedStateFromProps(props, state) {
-    if (state.sliderState !== State.ACTIVE) return {bounds: props.bounds}
-    else return null
+    if (state.sliderState !== State.ACTIVE && state.bounds !== props.bounds) {
+      return {
+        bounds: props.bounds
+      }
+    } else return null
   }
 
   get computedPosition() {
@@ -45,45 +48,14 @@ export default class MarkerContainer extends Component {
 
   shouldComponentUpdate(nextProps, nextState) {
     return (
-      (this.state.sliderState !== State.ACTIVE &&
-        (nextProps.position !== this.props.position ||
-          nextState.bounds !== this.state.bounds ||
-          nextState.layout !== this.state.layout)) ||
+      nextProps.position !== this.props.position ||
+      nextState.bounds !== this.state.bounds ||
+      nextState.layout !== this.state.layout ||
       Object.keys(nextProps.children.props).findIndex(
         (key) =>
           nextProps.children.props[key] !== this.props.children.props[key]
       ) !== -1
     )
-  }
-
-  get hitSlop() {
-    const {hitSlop} = this.props
-    const {bounds} = this.state
-    if (isNaN(hitSlop)) return hitSlop
-    return {
-      top: hitSlop,
-      bottom: hitSlop,
-      left: Math.min(Math.abs(this.offset - bounds.left) / 2, hitSlop),
-      right: Math.min(bounds.right / 2, hitSlop)
-    }
-  }
-
-  get handlerStyle() {
-    const {zIndex, index} = this.props
-    const {layout} = this.state
-    if (!layout) return {opacity: 0}
-    return {
-      zIndex: index + zIndex,
-      opacity: 1,
-      transform: [
-        {
-          translateX: Animated.add(
-            this.computedPosition,
-            new Animated.Value(-layout.width / 2)
-          )
-        }
-      ]
-    }
   }
 
   onLayout = ({
@@ -109,8 +81,49 @@ export default class MarkerContainer extends Component {
     }
   }
 
+  get hitSlop() {
+    const {hitSlop} = this.props
+    const {bounds} = this.state
+    if (isNaN(hitSlop)) return hitSlop
+    return {
+      top: hitSlop,
+      bottom: hitSlop,
+      left: Math.min(Math.abs(this.offset - bounds.left) / 2, hitSlop),
+      right: Math.min(bounds.right / 2, hitSlop)
+    }
+  }
+
+  get paddingAndroid() {
+    const hitSlop = this.hitSlop
+    const style = {}
+    Object.keys(hitSlop).forEach((key) => {
+      style[`padding${key[0].toUpperCase()}${key.slice(1)}`] = hitSlop[key]
+    })
+    style.marginTop = -hitSlop.top
+    style.marginLeft = -hitSlop.left
+    return style
+  }
+
+  get handlerStyle() {
+    const {zIndex, index} = this.props
+    const {layout} = this.state
+    if (!layout) return {opacity: 0}
+    return {
+      zIndex: index + zIndex,
+      opacity: 1,
+      transform: [
+        {
+          translateX: Animated.add(
+            this.computedPosition,
+            new Animated.Value(-layout.width / 2)
+          )
+        }
+      ]
+    }
+  }
+
   render() {
-    const {children, useNativeDriver, ...props} = this.props
+    const {children, useNativeDriver, position, value, ...props} = this.props
     const {bounds} = this.state
     delete props.hitSlop
     return (
@@ -122,9 +135,17 @@ export default class MarkerContainer extends Component {
         <Animated.View
           useNativeDriver={useNativeDriver}
           hitSlop={this.hitSlop}
-          style={[{position: 'absolute'}, this.handlerStyle]}
+          style={[
+            {position: 'absolute'},
+            this.handlerStyle,
+            Platform.OS === 'android' && this.paddingAndroid
+          ]}
         >
-          {React.cloneElement(children, {onLayout: this.onLayout})}
+          <View style={{flex: 0}} onLayout={this.onLayout}>
+            {React.cloneElement(children, {
+              marker: {position, value}
+            })}
+          </View>
         </Animated.View>
       </PanGestureHandler>
     )
