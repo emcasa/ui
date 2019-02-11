011a881b89142153b9c3ec03826942d8f74057a5
Gabriela Seabra <gabriela.lima.se@gmail.com>
2019-02-11 17:18:59 -0200

    Breakpoint helper documentation

---
+++ docs/helpers/breakpoint.js
@@ -0,0 +1,16 @@
+import styled from 'styled-components'
+import {breakpoint} from '@emcasa/ui/src/styles'
+import {themeGet} from 'styled-system'
+import TextBase from '@emcasa/ui-dom/components/Text'
+
+export const Text = styled(TextBase)`
+  @media only screen and ${breakpoint.only('phone')} {
+    font-style: italic;
+  }
+  @media only screen and ${breakpoint.down('tablet')} {
+    color: ${themeGet('colors.pink')};
+  }
+  @media only screen and ${breakpoint.up('desktop')} {
+    color: ${themeGet('colors.blue')};
+  }
+`
