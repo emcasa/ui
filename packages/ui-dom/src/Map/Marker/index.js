b35376e73f02065687b52ba637e4aafc3d4f66e7
Gabriela Seabra <gabriela.lima.se@gmail.com>
2019-02-22 21:09:34 -0300

    Detault marker mouse events

--- packages/ui-dom/src/Map/Marker/index.js
+++ packages/ui-dom/src/Map/Marker/index.js
@@ -4,13 +4,23 @@ import Container from './styles'
 
 export default class MapMarker extends PureComponent {
   render() {
-    const {id, lat, lng, children, onSelect, highlight} = this.props
+    const {
+      id,
+      lat,
+      lng,
+      children,
+      onSelect,
+      highlight,
+      onClick = () => onSelect && onSelect(id),
+      onMouseEnter = () => onSelect && onSelect(id, {lat, lng}),
+      onMouseLeave = () => onSelect && onSelect(id, {})
+    } = this.props
 
     return (
       <Container
-        onClick={() => onSelect && onSelect(id)}
-        onMouseEnter={() => onSelect && onSelect(id, {lat, lng})}
-        onMouseLeave={() => onSelect && onSelect(id, {})}
+        onClick={onClick}
+        onMouseEnter={onMouseEnter}
+        onMouseLeave={onMouseLeave}
         lat={lat}
         lng={lng}
         className={classNames({highlight, text: typeof children === 'string'})}
