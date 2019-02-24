d8e29436fcc1de2745acdd31ada7681623e23279
Gabriela Seabra <gabriela.lima.se@gmail.com>
2019-02-24 18:36:08 -0300

    Add breakpointNames to theme config

--- packages/ui-dom/src/Breakpoint/index.js
+++ packages/ui-dom/src/Breakpoint/index.js
@@ -6,11 +6,10 @@ import {canUseDOM} from 'fbjs/lib/ExecutionEnvironment'
 import PropTypes from 'prop-types'
 import {withTheme} from 'styled-components'
 import {breakpoint} from '@emcasa/ui/lib/styles'
-import {BREAKPOINTS} from '@emcasa/ui/lib/theme/measures'
 import Media from 'react-responsive'
 
 export const BreakpointType = PropTypes.oneOfType([
-  PropTypes.oneOf(Array.from(BREAKPOINTS.keys())),
+  PropTypes.string,
   PropTypes.number
 ])
 
@@ -42,76 +41,79 @@ export const getWindowWidth = () => {
     return window.innerWidth || document.documentElement.clientWidth
 }
 
-export const getBreakpoint = (windowWidth) => {
-  let prev
+export const getBreakpoint = ({width: windowWidth, theme}) => {
+  let index = 0,
+    name
   const width = windowWidth || getWindowWidth()
-  const iterator = BREAKPOINTS[Symbol.iterator]()
-  for (const [name, bpWidth] of iterator) {
-    if (parseInt(bpWidth) >= width) break
-    prev = name
+  for (; index < theme.breakpointNames.length; ++index) {
+    name = theme.breakpointNames[index]
+    if (parseInt(theme.breakpoints[index]) >= width) break
   }
   return {
-    breakpoint: prev,
-    isMobile: breakpoint.isMobile(prev)
+    breakpoint: name,
+    isMobile: breakpoint.isMobile(name)
   }
 }
 
-export class BreakpointProvider extends PureComponent {
-  static propTypes = {
-    children: PropTypes.func.isRequired,
-    debounce: PropTypes.number.isRequired,
-    disabled: PropTypes.bool
-  }
+export const BreakpointProvider = withTheme(
+  class BreakpointProvider extends PureComponent {
+    static propTypes = {
+      children: PropTypes.func.isRequired,
+      debounce: PropTypes.number.isRequired,
+      disabled: PropTypes.bool
+    }
 
-  static defaultProps = {
-    debounce: 150
-  }
+    static defaultProps = {
+      debounce: 150
+    }
 
-  state = {
-    breakpoint: undefined,
-    isMobile: undefined
-  }
+    state = {
+      breakpoint: undefined,
+      isMobile: undefined
+    }
 
-  constructor(props) {
-    super(props)
-    this.update = debounce(this._update, props.debounce)
-    if (canUseDOM && !props.disabled) this.state = getBreakpoint()
-  }
+    constructor(props) {
+      super(props)
+      this.update = debounce(this._update, props.debounce)
+      if (canUseDOM && !props.disabled) this.state = getBreakpoint(props)
+    }
 
-  _addEvents = () => {
-    window.addEventListener('resize', this.update)
-  }
+    _addEvents = () => {
+      window.addEventListener('resize', this.update)
+    }
 
-  _removeEvents = () => {
-    window.removeEventListener('resize', this.update)
-  }
+    _removeEvents = () => {
+      window.removeEventListener('resize', this.update)
+    }
 
-  _update = () => this.setState(getBreakpoint())
+    _update = () => this.setState(getBreakpoint(this.props))
 
-  componentDidUpdate(prevProps) {
-    if (prevProps.debounce !== this.props.debounce) {
-      this._removeEvents()
-      this.update.cancel()
-      this.update = debounce(this._update, this.props.debounce)
-      this._addEvents()
-    } else if (prevProps.disabled !== this.props.disabled) {
-      if (!this.props.disabled) this._addEvents()
-      else this._removeEvents()
+    componentDidUpdate(prevProps) {
+      if (prevProps.debounce !== this.props.debounce) {
+        this._removeEvents()
+        this.update.cancel()
+        this.update = debounce(this._update, this.props.debounce)
+        this._addEvents()
+      } else if (prevProps.disabled !== this.props.disabled) {
+        if (!this.props.disabled) this._addEvents()
+        else this._removeEvents()
+      }
+      if (prevProps.width !== this.props.width) this._update()
+    }
+
+    componentDidMount() {
+      if (canUseDOM && !this.props.disabled) this._addEvents()
+    }
+
+    componentWillUnmount() {
+      if (canUseDOM && !this.props.disabled) this._removeEvents()
+    }
+
+    render() {
+      return this.props.children(this.state)
     }
   }
-
-  componentDidMount() {
-    if (canUseDOM && !this.props.disabled) this._addEvents()
-  }
-
-  componentWillUnmount() {
-    if (canUseDOM && !this.props.disabled) this._removeEvents()
-  }
-
-  render() {
-    return this.props.children(this.state)
-  }
-}
+)
 
 export const withBreakpoint = (getOptions = {}) => (Target) =>
   hoistStatics(
--- packages/ui/src/index.js
+++ packages/ui/src/index.js
@@ -12,6 +12,7 @@ import {
 export default {
   fontFamily: 'FaktSoftPro-Normal',
   breakpoints: Array.from(BREAKPOINTS.values()),
+  breakpointNames: Array.from(BREAKPOINTS.keys()),
   space: [0, 4, 10, 14, 20, 25, 80],
   fontSizes: Array.from(FONT_SIZE.values()),
   letterSpacing: Array.from(LETTER_SPACING.values()),
