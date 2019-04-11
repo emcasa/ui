fcc6e8a9bef39c91c1b31a3dce90995264fa670c
Luis Gustavo Beligante <luis.beligante@emcasa.com>
2019-04-11 17:53:32 -0300

    Fix reference passed to filters. Removing unkown char from price format

--- packages/ui-dom/src/Filters/Filter.js
+++ packages/ui-dom/src/Filters/Filter.js
@@ -106,14 +106,14 @@ export default class Filter extends PureComponent {
     } else {
       return (
         <Manager>
-          <Reference>{({ref}) => this.renderButton({innerRef: ref})}</Reference>
+          <Reference>{({ref}) => this.renderButton({ref})}</Reference>
           {ReactDOM.createPortal(
             <Popper
               positionFixed
               placement="bottom-start"
               modifiers={popperModifiers}
             >
-              {({ref, style}) => this.renderPanel({innerRef: ref, style})}
+              {({ref, style}) => this.renderPanel({ref, style})}
             </Popper>,
             contentRef.current
           )}
