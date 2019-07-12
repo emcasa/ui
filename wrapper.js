6f0f421434a53145a8c47ef8ba711a63bea7a342
Luis Gustavo Beligante <luis.beligante@emcasa.com>
2019-07-12 08:34:54 -0300

    refac(icons): remove default icons from theme object

--- wrapper.js
+++ wrapper.js
@@ -1,7 +1,14 @@
 import React from 'react'
 import {ThemeProvider, createGlobalStyle} from 'styled-components'
 import EmCasaStyles from '@emcasa/ui-dom/src/global-styles'
+import {fas} from '@fortawesome/free-solid-svg-icons'
+
 import theme from '@emcasa/ui'
+
+const iconedTheme = {
+  ...theme,
+  icons: {default: fas}
+}
 
 const DoczStyles = createGlobalStyle`
   * {
@@ -14,7 +21,7 @@ const DoczStyles = createGlobalStyle`
  * Docz Wrapper.
  */
 const Wrapper = ({children}) => (
-  <ThemeProvider theme={theme}>
+  <ThemeProvider theme={iconedTheme}>
     <>
       <DoczStyles />
       <EmCasaStyles />
