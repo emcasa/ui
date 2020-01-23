e57663eb6123344c3f4c3e4ae97b8a9f4e7fa879
Luis Gustavo <luigugah@gmail.com>
2020-01-23 11:52:04 -0300

    feat(@emcasa/login): add a pure login version where we don't need redux saga to handle the mutations

---
+++ packages/login/index.js
@@ -0,0 +1,5 @@
+import LoginSaga from '@/components/login/LoginSaga'
+import LoginPure from '@/components/login/LoginPure'
+import authSaga from '@/redux/modules/auth/saga'
+
+export default {LoginSaga, LoginPure, authSaga}
