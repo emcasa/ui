2ffbd8421a1bbeee7329ef16ac4466760f5058f0
jrgarou <jrgarou@gmail.com>
2021-10-25 17:23:59 -0300

    feat(ui-dom/Dropdown): update markup and styles

--- packages/ui-dom/src/Dropdown/Container.js
+++ packages/ui-dom/src/Dropdown/Container.js
@@ -1,65 +1,94 @@
 import React, {PureComponent} from 'react'
 import ReactDOM from 'react-dom'
 import styled from 'styled-components'
+import {themeGet} from '@styled-system/theme-get'
+import theme from '@emcasa/ui'
 import {container} from '@emcasa/ui/lib/components/Dropdown'
 
-export default styled(
-  class DropdownContainer extends PureComponent {
-    static propTypes = container.propTypes
+const MAX_HEIGHT = theme.buttonHeight[0] * 3 + theme.buttonHeight[0] / 2
 
-    get targetDOMNode() {
-      return this.props.target
-        ? ReactDOM.findDOMNode(this.props.target)
-        : undefined
-    }
-
-    componentDidMount() {
-      document.addEventListener('click', this.onDocumentClick)
-      document.addEventListener('touchend', this.onDocumentClick)
-      document.addEventListener('focus', this.onDocumentFocus, true)
-    }
-
-    componentWillUnmount() {
-      document.removeEventListener('click', this.onDocumentClick)
-      document.removeEventListener('touchend', this.onDocumentClick)
-      document.removeEventListener('focus', this.onDocumentFocus, true)
-    }
-
-    onDocumentClick = (e) => {
-      const {focused, onDropdownBlur} = this.props
-      const target = this.targetDOMNode
-      if (focused && target && onDropdownBlur && target && !target.contains(e.target)) {
-        onDropdownBlur()
-      }
-    }
-
-    onDocumentFocus = (e) => {
-      const {focused, onDropdownBlur, onDropdownFocus} = this.props
-      const target = this.targetDOMNode
-      if (focused && target && !target.contains(e.target)) onDropdownBlur()
-      else if (!focused && target && target.contains(e.target)) onDropdownFocus()
-    }
-
-    render() {
-      const {children, id, className, style} = this.props
-      return (
-        <div tabIndex="-1" id={id} className={className} style={style}>
-          {children}
-        </div>
-      )
-    }
-  }
-)`
+const Listbox = styled.ul`
   z-index: -1;
   position: absolute;
-  top: 100%;
+  left: 0;
+  top: calc(100% + ${themeGet('space.1')}px);
   width: 100%;
   overflow-y: auto;
-  ${container};
-  ${container.contentContainer};
-  display: ${({focused}) => (focused ? 'flex' : 'none')};
+  max-height: ${MAX_HEIGHT}px;
+  pointer-events: ${({focused}) => (focused ? 'initial' : 'none')};
+  opacity: ${({focused}) => (focused ? '1' : '0')};
+  transform: ${({focused}) =>
+    focused ? 'translateY(0)' : 'translateY(-12px)'};
+  transition: opacity ${({focused}) => (focused ? '0.2s' : '0.15s')} linear,
+    transform 0.5s cubic-bezier(0.4, 0.2, 0, 1);
+  display: flex;
+  flex-direction: column;
   box-sizing: border-box;
+  background-color: ${themeGet('colors.white')};
+  border: 1px solid ${themeGet('colors.grey300')};
+  border-radius: 8px;
+  box-shadow: 0 8px 14px rgba(131, 145, 165, 0.2);
+  margin: 0;
+  padding: 0;
   &:focus {
     outline: none;
   }
 `
+
+export default class DropdownContainer extends PureComponent {
+  static propTypes = container.propTypes
+
+  get targetDOMNode() {
+    return this.props.target
+      ? ReactDOM.findDOMNode(this.props.target)
+      : undefined
+  }
+
+  componentDidMount() {
+    document.addEventListener('click', this.onDocumentClick)
+    document.addEventListener('touchend', this.onDocumentClick)
+    document.addEventListener('focus', this.onDocumentFocus, true)
+  }
+
+  componentWillUnmount() {
+    document.removeEventListener('click', this.onDocumentClick)
+    document.removeEventListener('touchend', this.onDocumentClick)
+    document.removeEventListener('focus', this.onDocumentFocus, true)
+  }
+
+  onDocumentClick = (e) => {
+    const {focused, onDropdownBlur} = this.props
+    const target = this.targetDOMNode
+    if (
+      focused &&
+      target &&
+      onDropdownBlur &&
+      target &&
+      !target.contains(e.target)
+    ) {
+      onDropdownBlur()
+    }
+  }
+
+  onDocumentFocus = (e) => {
+    const {focused, onDropdownBlur, onDropdownFocus} = this.props
+    const target = this.targetDOMNode
+    if (focused && target && !target.contains(e.target)) onDropdownBlur()
+    else if (!focused && target && target.contains(e.target)) onDropdownFocus()
+  }
+
+  render() {
+    const {children, dropdownId, focused} = this.props
+    return (
+      <Listbox
+        id={`${dropdownId}list`}
+        focused={focused}
+        tabIndex="-1"
+        role="listbox"
+        aria-labelledby="exp_elem"
+      >
+        {children}
+      </Listbox>
+    )
+  }
+}
--- packages/ui-dom/src/Dropdown/Option.js
+++ packages/ui-dom/src/Dropdown/Option.js
@@ -1,33 +1,67 @@
 import React from 'react'
-import styled from 'styled-components'
+import styled, {css} from 'styled-components'
+import {themeGet} from '@styled-system/theme-get'
 import {option} from '@emcasa/ui/lib/components/Dropdown'
 
-const DropdownOption = styled(({children, id, className, style, onSelect}) => {
-  return (
-    <button
-      type="button"
-      tabIndex={0}
-      id={id}
-      className={className}
-      style={style}
-      onMouseDown={onSelect}
-    >
-      {children}
-    </button>
-  )
-})`
+const Button = styled.li`
+  display: flex;
+  align-items: center;
+  flex: 0 0 ${themeGet('buttonHeight.0')}px;
+  padding: 0 ${themeGet('space.4')}px;
+  box-sizing: border-box;
+  overflow: hidden;
+  font-size: ${themeGet('buttonFontSize.0')}px;
+  color: ${themeGet('colors.grey900')};
+  line-height: 20px;
+  text-align: left;
+  background-color: ${themeGet('colors.white')};
   outline: none;
   cursor: pointer;
-  ${option};
-  ${option.text};
-  overflow: hidden;
-  white-space: nowrap;
-  text-overflow: ellipsis;
   &:hover,
   &:focus {
-    ${option.hover};
+    background-color: ${themeGet('colors.grey100')};
   }
 `
+
+const Radio = styled.span`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex: 0 0 22px;
+  height: 22px;
+  margin-right: ${themeGet('space.3')}px;
+  border: 1px solid ${themeGet('colors.grey900')};
+  border-radius: 100%;
+
+  &::after {
+    content: '';
+    width: 16px;
+    height: 16px;
+    background-color: ${themeGet('colors.grey900')};
+    transition: transform 0.45s cubic-bezier(0.4, 0.2, 0, 1);
+    border-radius: 100%;
+    ${({selected}) =>
+      selected
+        ? css`
+            transform: scale(1);
+          `
+        : css`
+            transform: scale(0);
+          `}
+  }
+`
+
+const DropdownOption = ({children, selected, onSelect}) => (
+  <Button
+    role="option"
+    aria-selected={selected}
+    tabIndex={0}
+    onMouseDown={onSelect}
+  >
+    <Radio selected={selected} />
+    {children}
+  </Button>
+)
 
 DropdownOption.displayName = 'DropdownOption'
 
